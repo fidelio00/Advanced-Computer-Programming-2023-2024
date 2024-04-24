10e8d011ed0ef455085e5716581c23cf37223192
fidelio00 <164526100+fidelio00@users.noreply.github.com>
2024-04-24 11:14:01 +0200

    Update 07_Multiprocess.py

--- 07_concurrency/07_Multiprocess.py
+++ 07_concurrency/07_Multiprocess.py
@@ -51,3 +51,92 @@ if __name__ == '__main__':
 
     # wait until the process finishes
     p.join()
+
+--------------------------------------------------------------------------------------------------------------------
+
+from multiprocess import Process, Lock
+
+def f(l, i):
+    l.acquire()
+    try:
+        print('hello world', i)
+    finally:
+        l.release()
+
+if __name__ == '__main__':
+    lock = Lock()
+    for num in range(10):
+        Process(target=f, args=(lock, num)).start()
+
+---------------------------------------------------------------------------------------------------------------------
+
+### uso di multiprocess Pipe
+
+"""
+Una Pipe in Python è un meccanismo di comunicazione bidirezionale che permette lo scambio di dati tra processi. 
+È costituita da due endpoint, uno per il processo genitore e uno per il processo figlio. 
+I processi possono scrivere dati in un endpoint e leggerli dall'altro. 
+La classe Pipe del modulo multiprocessing crea una Pipe e restituisce due oggetti connessione che rappresentano i due endpoint.
+
+"""
+
+from multiprocess import Process, Pipe
+
+def parentData(parent):
+    parent.send(['Hello'])
+    parent.close()
+    
+def childData(child):
+    child.send(['Bye'])
+    child.close()
+
+if __name__ == '__main__':
+    parent_conn, child_conn = Pipe()
+    p1 = Process(target=parentData, args=(parent_conn,))
+    p1.start()
+    p2 = Process(target=childData, args=(child_conn,))
+    p2.start()
+    print(parent_conn.recv()) #il parent riceve Bye
+    print(child_conn.recv())  #il child riceve Hello 
+    p1.join()
+    p2.join()
+
+-------------------------------------------------------------------------------------------------------------------------
+
+## uso di multiprocess Queue
+
+from multiprocess import Process, Queue
+
+def f(q):
+    q.put([42, None, 'hello'])
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=f, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+
+----------------------------------------------------------------------------------------------------------------------------
+
+### uso di multiprocess shared memory
+
+# Value viene utilizzata per creare una variabile condivisa tra i processi 
+# Array viene utilizzata per creare un array condiviso tra i processi.
+from multiprocess import Process, Value, Array
+
+def f(n, a):
+    n.value = 3.1415927
+    for i in range(len(a)):
+        a[i] = -a[i]
+
+if __name__ == '__main__':
+    num = Value('d', 0.0)
+    arr = Array('i', range(10))
+    p = Process(target=f, args=(num, arr))
+    p.start()
+    p.join()
+    print(num.value)
+    print(arr[:])
+
+
